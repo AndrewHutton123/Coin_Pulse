ff163f04358221ba0fcf6fd79aec5176824079bf
AndrewHutton123 <andrewhu11on123@gmail.com>
2026-01-18 18:17:02 +0000

    candle stick chart displaying data across different time frames

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,37 +1,51 @@
+/* eslint-disable react-hooks/error-boundaries */
 import { fetcher } from "@/lib/coingecko.actions";
 import { formatCurrency } from "@/lib/utils";
 import Image from "next/image";
 import { CoinOverviewFallback } from "./Fallback";
+import CandleStickChart from "../CandleStickChart";
 
 const CoinOverview = async () => {
-  let coinData: CoinDetailsData;
   try {
-    coinData = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-      dex_pair_format: "symbol",
-    });
+    const [coinData, coinOHLCData] = await Promise.all([
+      await fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+
+      await fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: "1",
+      }),
+    ]);
+
+    return (
+      <div id="coin-overview">
+        <CandleStickChart
+          data={coinOHLCData}
+          coinId="bitcoin"
+          liveInterval="1m"
+        >
+          <div className="header pt-2">
+            <Image
+              src={coinData.image.large}
+              alt={coinData.name}
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>
+                {coinData.name} / {coinData.symbol.toUpperCase()}
+              </p>
+              <h1>{formatCurrency(coinData.market_data.current_price.usd)}</h1>
+            </div>
+          </div>
+        </CandleStickChart>
+      </div>
+    );
   } catch (error) {
     console.error("Error fetching coin:", error);
     return <CoinOverviewFallback />;
   }
-
-  return (
-    <div id="coin-overview">
-      <div className="header pt-2">
-        <Image
-          src={coinData.image.large}
-          alt={coinData.name}
-          width={56}
-          height={56}
-        />
-        <div className="info">
-          <p>
-            {coinData.name} / {coinData.symbol.toUpperCase()}
-          </p>
-          <h1>{formatCurrency(coinData.market_data.current_price.usd)}</h1>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default CoinOverview;
